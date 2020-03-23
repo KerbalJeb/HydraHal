36e060abee7f9f1ab478183b65a2ba615368b124
Ben Kellman <kellman.ben@gmail.com>
2020-03-22 19:13:53 -0700

    Updated reg.hpp documentation

--- inc/hydra_hal/core/reg.hpp
+++ inc/hydra_hal/core/reg.hpp
@@ -14,7 +14,7 @@ namespace {
             volatile std::uint32_t *reg,
             std::uint32_t mask,
             std::uint32_t offset) {
-        return *reg;
+        return (*reg & ~(mask << offset)) >> offset;
     }
 
     inline void _write(
@@ -25,8 +25,13 @@ namespace {
         *reg = (*reg & ~(mask << offset)) | ((value & mask) << offset);
     }
 }
-
+/*!
+ * \brief Contains basic types that should not need to be used directly
+ */
 namespace hydra::core {
+    /*!
+     * \brief Read only mutability
+     */
     struct ro_t {
         static inline std::uint32_t read(
                 volatile std::uint32_t *reg,
@@ -36,6 +41,9 @@ namespace hydra::core {
         }
     };
 
+    /*!
+     * \brief Write only mutability
+     */
     struct wo_t {
         static inline void write(
                 volatile std::uint32_t *reg,
@@ -46,6 +54,9 @@ namespace hydra::core {
         }
     };
 
+    /*!
+     * \brief Read-Write mutability
+     */
     struct rw_t {
         static inline void write(
                 volatile std::uint32_t *reg,
@@ -64,11 +75,11 @@ namespace hydra::core {
     };
 
     /*!
-     * \brief Represents a single field in a perpheral register
-     * @tparam base_address
-     * @tparam mask
-     * @tparam offset
-     * @tparam mutability_policy
+     * \brief Represents a single field in a peripheral register
+     * @tparam base_address The base address of the register
+     * @tparam mask Non offset bit mask
+     * @tparam offset Offset of mask from zero bit
+     * @tparam mutability_policy Provides read and/or write functions
      */
     template
             <
@@ -78,6 +89,11 @@ namespace hydra::core {
                     class mutability_policy
             >
     struct bit_field_t {
+        /*!
+         * \brief Writes to reg using mutability policy, templated to static cast to uint_32 to simplify writing with enums
+         * @tparam T Type of value (must be castable to int)
+         * @param value Value to write
+         */
         template<typename T>
         inline static void write(T const value) {
             mutability_policy::write(
@@ -87,6 +103,10 @@ namespace hydra::core {
                     static_cast<std::uint32_t>(value));
         }
 
+        /*!
+         * \brief Reads from reg using mutability policy
+         * @return The value of the bit field (shifted to have zero offset)
+         */
         inline static std::uint32_t read() {
             return mutability_policy::read(
                     reinterpret_cast<volatile std::uint32_t *>(base_address),
@@ -95,6 +115,14 @@ namespace hydra::core {
         }
     };
 
+    /*!
+     * \brief Used to represent a series of sequential bit fields (such as those in GPIO registers)
+     * @tparam base_address The register base address
+     * @tparam width The width of each field
+     * @tparam offset The offset of the first field
+     * @tparam total_fields The total number of fields
+     * @tparam mutability_policy
+     */
     template
             <
                     std::uint32_t base_address,
@@ -104,6 +132,12 @@ namespace hydra::core {
                     class mutability_policy
             >
     struct bit_field_group_t {
+        /*!
+         * \brief Writes to a single bit field in a block of sequential bit fields (ie. GPIO mode)
+         * @tparam T
+         * @param idx The index of the bit field to write to
+         * @param value
+         */
         template<typename T>
         inline static void write(std::uint32_t idx, T const value) {
             mutability_policy::write(
@@ -113,12 +147,18 @@ namespace hydra::core {
                     static_cast<std::uint32_t>(value));
         }
 
+        /*!
+         * \brief Reads from a single bit field in a block of sequential bit fields
+         * @param idx The index of the bit field to read from
+         * @return
+         */
         inline static std::uint32_t read(std::uint32_t idx) {
             return mutability_policy::read(
                     reinterpret_cast<volatile std::uint32_t *>(base_address) + (idx * width / 32),
                     mask,
                     offset + idx * width);
         }
+
     private:
         static constexpr std::uint32_t mask = (1 << width) - 1;
     };
